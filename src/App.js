7386e37a526ef1330ff03e10bff3754a0841d23a
rhymemime <bramsey967@gmail.com>
2020-04-21 14:10:47 -0400

    some tweet stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react"; 
+import Tweet from "./Tweet"
 
 function App(){
 
   return(
     <div>
       <h1>Hello React</h1>
+      <Tweet />
+      <Tweet />
+      <Tweet />
       <Tweet />
     </div>
   );
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./App.css"
 
 function Tweet(){
     return(
-        <div>
+        <div className="tweet">
             <h3>Name</h3>
             <p>This is a random tweet</p>
             <h3>Number of Likes</h3>
