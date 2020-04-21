dd58775414f74b1a23a960bf6a86381af67facef
rhymemime <bramsey967@gmail.com>
2020-04-21 14:06:27 -0400

    Some tutorial learning

---
+++ src/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tweet(){
+    return(
+        <div>
+            <h3>Name</h3>
+            <p>This is a random tweet</p>
+            <h3>Number of Likes</h3>
+        </div>
+    )
+}
+
+export default Tweet;
