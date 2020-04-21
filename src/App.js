a9ee2027802db486a8bb7a4fa70f330acf365049
rhymemime <bramsey967@gmail.com>
2020-04-21 14:43:31 -0400

    some more tutorial stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react"; 
+import React, {useState} from "react"; 
 import Tweet from "./Tweet"
 
 function App(){
+  const [isRed, setRed] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const [users,setUsers] = useState([
+    {name: 'Ben', message: 'Hi!'},
+    {name: 'Ben', message: 'ancient evils'},
+    {name: 'Ben', message: 'nope.avi'},
+    {name: 'Ben', message: 'l4d2'},
+    {name: 'Ben', message: 'no'},
+    {name: 'Alizah', message: 'wuh'},
+  ])
+
+  const Increment = () => {
+    setCount (count + 1);
+    setRed(!isRed);
+  }
 
   return(
     <div>
-      <h1>Hello React</h1>
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
+      <h1 className={isRed ? 'red' : ''}>Change my color!</h1>
+      <button onClick={Increment}>Increment</button>
+      <h1>{count}</h1>
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./App.css"
 
-function Tweet(){
+function Tweet(props){
     return(
         <div className="tweet">
-            <h3>Name</h3>
-            <p>This is a random tweet</p>
+            <h3>{props.name}</h3>
+            <p>{props.message}</p>
             <h3>Number of Likes</h3>
         </div>
     )
